4276251dd6429f8f89aa3dcfc404fbcf3e8d74f4
duarteCost <2084813@max.student.pt>
2017-01-12 15:49:23 +0000

    controller

---
+++ MSALConnect/Controllers/DoubtsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MSALConnect.Controllers
+{
+    public class DoubtsController : Controller
+    {
+        // GET: Doubts
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Course_doubt(int id)
+        {
+            return View();
+        }
+    }
+}
---
+++ MSALConnect/Controllers/ProjectsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MSALConnect.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        // GET: Projects
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Course_Projects(int id)
+        {
+            return View();
+        }
+    }
+}
