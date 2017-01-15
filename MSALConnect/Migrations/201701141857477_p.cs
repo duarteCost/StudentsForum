46bfbbf1d40e7580b15a759d5942277a29bd6c85
duarteCost <2084813@max.student.pt>
2017-01-15 09:49:50 +0000

    aaaaaaaas

---
+++ MSALConnect/Migrations/201701141857477_p.cs
@@ -0,0 +1,16 @@
+namespace MSALConnect.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class p : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
