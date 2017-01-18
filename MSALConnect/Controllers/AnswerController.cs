46f5c9761c267eac381ad2fb50c8e5019d3e705a
Pedro <Pedro@DESKTOP-1U5SBR3>
2017-01-18 16:17:50 +0000

    Não Há Mais!

--- MSALConnect/Controllers/AnswerController.cs
+++ MSALConnect/Controllers/AnswerController.cs
@@ -53,7 +53,6 @@ namespace MSALConnect.Controllers
                 AnswerFile file1 = new AnswerFile() { name = fileName, filePath = path, answer = answer };
                 db.AnswerFiles.Add(file1);
                 db.SaveChanges();
-                //answerFilesList = db.AnswerFiles.ToList();
             }
 
             db.SaveChanges();
@@ -61,47 +60,6 @@ namespace MSALConnect.Controllers
         }
 
 
-        /*
-        [HttpPost]
-        public ActionResult CreateAnswer(HttpPostedFileBase file, string content, int id, int Course_id, int view)
-        {
-            var user_id = Session["userNumber"];
-            Answer answer = new Answer();
-            var question = db.Doubts.Find(id);
-            answer.content = content;
-            DateTime localDate = DateTime.Now;
-            answer.student = db.Students.Find(user_id);
-            answer.date = localDate;
-            answer.doubts = question;
-            db.Answers.Add(answer);
-
-            if (file != null && file.ContentLength > 0)
-            {
-                var fileName = Path.GetFileName(file.FileName);
-                var path = Path.Combine(Server.MapPath("~/App_Data/uploads"), fileName);
-                file.SaveAs(path);
-                AnswerFile file1 = new AnswerFile() { name = fileName, filePath = path, answer = answer };
-                db.AnswerFiles.Add(file1);
-                db.SaveChanges();
-                answerFilesList = db.AnswerFiles.ToList();
-            }
-            db.SaveChanges();
-
-
-
-            if (view == 0)
-            {
-                return RedirectToAction("Create", "Doubt", new { id = Course_id });
-            }
-            else
-            {
-
-                return RedirectToAction("Index", new { Course_id = Course_id , id = id });
-            }
-
-        }
-
-    */
-
+       
     }
 }
